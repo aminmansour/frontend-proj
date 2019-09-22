6bbf6c9690a839f95210b88ef18d2676caf71d91
Amin mnasour <amin.mansour@kcl.ac.uk>
2019-09-22 19:06:11 +0100

    add context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
+import pf from "petfinder-client";
+import { Provider } from "./SearchContext";
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
+const petfinderClient = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "Seattle, WA",
+      animal: "",
+      breed: "",
+      breeds: [],
+      handleAnimalChange: this.handleAnimalChange,
+      handleBreedChange: this.handleBreedChange,
+      handleLocationChange: this.handleAnimalChange,
+      getBreeds: this.getBreeds
+    };
+  }
+
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
+  handleAnimalChange = event => {
+    this.setState(
+      {
+        animal: event.target.value,
+        breed: ""
+      },
+      this.getBreed
+    );
+  };
+
+  handleBreedChange = event => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+
+  getBreed() {
+    if (this.state.animal) {
+      petfinderClient.breed
+        .list({ animal: this.state.animal })
+        .then(data => {
+          if (
+            data.petfinder &&
+            data.petfinder.breeds &&
+            Array.isArray(data.petfinder.breeds.breed)
+          ) {
+            this.setState({
+              breeds: data.petfinder.breeds.breed
+            });
+          } else {
+            this.setState({
+              breeds: []
+            });
+          }
+        })
+        .catch(err => console.error(err));
+    } else {
+      this.setState({
+        breeds: []
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <header>
           <Link to="/">Adopt me!</Link>
         </header>
-
-        <Router>
-          <SearchParams path="/search-params" />
-          <Results path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Provider value={this.state}>
+          <Router>
+            <SearchParams path="/search-params" />
+            <Results path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Provider>
       </div>
     );
   }
---
+++ src/SearchBox.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { ANIMALS } from "petfinder-client";
+import { Consumer } from "./SearchContext";
+
+class SearchBox extends React.Component {
+  render() {
+    return (
+      <Consumer>
+        {context => {
+          return (
+            <div className="search-params">
+              <form onSubmit="">
+                <label htmlFor="location">
+                  location
+                  <input
+                    onChange={context.handleLocationChange}
+                    id="location"
+                    placeholder="Location"
+                    value={context.location}
+                  />
+                </label>
+                <label htmlFor="animal">
+                  Animal
+                  <select
+                    onChange={context.handleAnimalChange}
+                    id="animal"
+                    placeholder="Animal"
+                    value={context.animal}
+                    onBlur={context.handleAnimalChange}
+                  >
+                    <option value="">Select An Animal</option>
+                    {ANIMALS.map(animal => (
+                      <option key={animal} value={animal}>
+                        {animal}
+                      </option>
+                    ))}
+                    ;
+                  </select>
+                </label>
+                <label htmlFor="breed">
+                  Breed
+                  <select
+                    onChange={context.handleBreedChange}
+                    id="breed"
+                    placeholder="Breed"
+                    value={context.breed}
+                    onBlur={context.handleBreedChange}
+                    disabled={context.animal === ""}
+                  >
+                    <option value="">Select An Breed</option>
+                    {context.breeds.map(breed => (
+                      <option key={breed} value={breed}>
+                        {breed}
+                      </option>
+                    ))}
+                    ;
+                  </select>
+                </label>
+
+                <button>Submit</button>
+              </form>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default SearchBox;
